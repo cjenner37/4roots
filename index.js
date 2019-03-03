0ef26677a1cdf30a8a53f4246786689a732594d0
Cameron Jenner <cjenner37@gmail.com>
2019-03-03 16:07:52 -0500

    update

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 $(document).ready(function () {
-	var navbar = $("#navbar");
-	if ($("#header").is('*')) {
-		var elem = $('#header');
-		var offset = elem.offset();
-		var leftValue = offset.left;
-		var topValue =  offset.top + elem.height();
-		var width = elem.width();
-	$(window).scroll(function (event) {
-		var y = $(this).scrollTop();
-		if (y >= topValue) {  
-		 if ($('#header').hasClass('fixed')){    
-		 }else{
-		    $('#header').addClass('fixed');
-		    $('#header').css({
-		        top: '-60px',
-		        width:width,
-		    });
-		     $('#header').animate({ 
-		    top: '0',
-		}, 500, function() {    
-	        });
-	}
-	} else {    
-	if ($('#header').hasClass('fixed')){        
-	$('#header').removeClass('fixed');
-	$('#header').fadeOut('fast', function(){ 
-	 $('#header').fadeIn('fast');
+	$(window).scroll(function() {
+	  if ($(document).scrollTop() > 100) {
+	    $('#navbar').addClass('color-change');
+	    $('.nav-item').addClass('height-change');
+	  } else {
+	    $('#navbar').removeClass('color-change');
+	    $('.nav-item').removeClass('height-change');
+	  }
 	});
-	    }
-	    }
-	  });
-	}
 })
